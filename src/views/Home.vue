170839cd1db41becdb90f8c5ecc518714009999a
fi0ti <pw.druida@gmail.com>
2021-03-12 16:33:19 -0300

    view home feita por Israel Cardos/ button, class CSS e methods feito por mim

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+   <h2 class="text-h5 text-center mb-3 mt-5">Brasileirão Série A</h2>
+    <v-img class="ini-img mb-4" src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png"/>
+    <p> A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí – Série A 2020 por motivos de patrocínio, é a 64.ª edição da principal divisão do futebol brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o sistema de pontos corridos.</p>
+    <p class="pb-4 text-h6 text-center mb-5"><strong >Se voce não curtir seu time vai cair!! </strong>{{cont}}</p>
+    </v-container>
+    <v-btn
+      class="mx-2"
+      fab
+      dark
+      small
+      color="teal darken-2"
+      @click="like"
+    >
+      <v-icon dark>
+        mdi-thumb-up
+      </v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data(){
+    return{
+      cont: 0
+    }
+  },
+  methods:{
+    like(){
+      this.cont ++
+    }
   }
 }
 </script>
+<style scoped>
+    .ini-img {
+        border-radius: 8px;
+    }
+</style>
